20fdf46971b69389ab1a83c22e2017a84e1c91b5
Mauricio Mercado <mau@maumercado.com>
2017-12-17 16:53:21 -0800

    Made Header Component reusable by passing props

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { AppRegistry, Text } from "react-native";
+import Header from "./src/components/header";
 
 export default class App extends Component {
     render() {
-        return <Text>Some Text</Text>;
+        return <Header headerText={"Albums"} />;
     }
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default class Header extends Component {
     render() {
-        return <Text>Header</Text>;
+        const { textStyle, viewStyle } = styles;
+
+        return (
+            <View style={viewStyle}>
+                <Text style={textStyle}>{this.props.headerText}</Text>
+            </View>
+        );
     }
 }
+
+const styles = {
+    viewStyle: {
+        backgroundColor: "#F8F8F8",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 60,
+        paddingTop: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: "relative"
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
