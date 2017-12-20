6e64da9d94029d8d7c2febb780ffacde6ae122aa
Mauricio Mercado <mau@maumercado.com>
2017-12-19 19:07:00 -0800

    Created initial setup for album details

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
+
 import Card from "./Card";
 import CardSection from "./CardSection";
+
 const AlbumDetail = ({ album }) => {
+    const { thumbnail_image, title, artist } = album;
+    const { headerContentStyle, thumbnailStyle } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>{album.title}</Text>
+                <View>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: "column",
+        justifyContent: "space-around"
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
